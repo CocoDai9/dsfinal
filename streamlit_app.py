653f821b4594e72fb0e760799e4e676c95accb24
CocoDai9 <md6132@nyu.edu>
2026-04-15 22:26:22 +0000

    converting cells into numerical values

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
+import pandas as pd
 import streamlit as st
+from sklearn.preprocessing import LabelEncoder
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+df = pd.read_csv("mushrooms.csv")
+
+label_encoders = {}
+
+df_encoded = df.copy()
+
+for column in df.columns:
+    le = LabelEncoder()
+    df_encoded[column] = le.fit_transform(df[column])
+    label_encoders[column] = le
+
+st.write("Encoded Dataset:")
+st.dataframe(df_encoded.head())
+
+st.write("Encoding Mapping:")
+
+for col in df.columns:
+    mapping = dict(zip(
+        label_encoders[col].classes_,
+        label_encoders[col].transform(label_encoders[col].classes_)
+    ))
+    
+    st.write(f"### {col}")
+    st.write(mapping)
